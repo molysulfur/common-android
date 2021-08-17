4d6539681f7e4ceeb61d6578bab7f7bdb4169943
Apirom Yaboon <molysulfur.mos@gmail.com>
2021-08-17 11:44:59 +0700

    config src minapi

---
+++ shared/src/main/java/com/awonar/android/shared/di/ApplicationModule.kt
@@ -0,0 +1,18 @@
+package com.awonar.android.shared.di
+
+import android.app.Application
+import com.awonar.android.shared.utils.HawkUtil
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideHawk(app: Application) = HawkUtil(app.applicationContext)
+}
