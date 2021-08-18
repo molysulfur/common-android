4766caf31ed80d3918faf34b6a71b4d4b0895d4b
Apirom Yaboon <molysulfur.mos@gmail.com>
2021-08-18 14:45:39 +0700

    base flow network

---
+++ src/main/java/com/molysulfur/library/network/FlowNetwork.kt
@@ -0,0 +1,44 @@
+package com.molysulfur.library.network
+
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import com.molysulfur.library.result.Result
+import com.molysulfur.library.vo.ApiEmptyResponse
+import com.molysulfur.library.vo.ApiErrorResponse
+import com.molysulfur.library.vo.ApiResponse
+import com.molysulfur.library.vo.ApiSuccessResponse
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
+
+abstract class FlowNetwork<RequestType, ResultType, ResponseType> {
+
+    fun asFlow() = flow {
+        emit(Result.Loading)
+        val response: ApiResponse<ResponseType> = ApiResponse.create(createCall())
+        when (response) {
+            is ApiSuccessResponse -> {
+                val result: ResultType = convertToResultType(response.body)
+                emit(Result.Success(result))
+            }
+
+            is ApiEmptyResponse -> {
+                emit(Result.Success(null))
+            }
+            is ApiErrorResponse -> {
+                onFetchFailed(response.errorMessage)
+                emit(Result.Error(Exception(response.errorMessage)))
+            }
+        }
+    }
+
+
+    @MainThread
+    protected abstract fun createCall(): Response<ResponseType>
+
+    @MainThread
+    protected abstract fun convertToResultType(response: ResponseType): ResultType
+
+    @WorkerThread
+    protected abstract fun onFetchFailed(errorMessage: String)
+
+}
