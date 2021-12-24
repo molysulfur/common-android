49ab929a9508f910824a48535de58d1f482e9cb5
molysulfur <molysulfur.mos@gmail.com>
2021-12-24 19:14:28 +0700

    userinfo graph

---
+++ app/src/main/java/com/awonar/app/ui/user/UserInfoFragment.kt
@@ -0,0 +1,6 @@
+package com.awonar.app.ui.user
+
+import androidx.fragment.app.Fragment
+
+class UserInfoFragment : Fragment() {
+}
---
+++ app/src/main/java/com/awonar/app/ui/user/adapter/UserInfoType.kt
@@ -0,0 +1,8 @@
+package com.awonar.app.ui.user.adapter
+
+object UserInfoType {
+    const val TITLE_USER_INFO = 0
+    const val SUBTITLE_USER_INFO = 1
+    const val TEXT_USER_INFO = 2
+    const val LOGO_USER_INFO = 3
+}
