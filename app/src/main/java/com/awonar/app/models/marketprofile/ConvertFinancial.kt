36a20b0bb3537759d962c3367155b16b06cc184a
molysulfur <molysulfur.mos@gmail.com>
2022-03-18 17:09:19 +0700

    financial income state selected item

---
+++ app/src/main/java/com/awonar/app/models/marketprofile/ConvertFinancial.kt
@@ -0,0 +1,14 @@
+package com.awonar.app.models.marketprofile
+
+import android.os.Parcelable
+import com.awonar.android.model.marketprofile.FinancialResponse
+import com.awonar.app.ui.marketprofile.stat.financial.FinancialMarketItem
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class ConvertFinancial(
+    val financial: FinancialResponse?,
+    val type: String?,
+    val defaultSet: List<FinancialMarketItem.BarEntryItem> = emptyList(),
+) : Parcelable
