7454f20b7be2e4701313723d90121a7ecf335338
molysulfur <molysulfur.mos@gmail.com>
2022-03-29 12:58:40 +0700

    on click item

--- app/src/main/java/com/awonar/app/ui/search/BindingAdapterSearch.kt
+++ app/src/main/java/com/awonar/app/ui/search/BindingAdapterSearch.kt
@@ -5,14 +5,15 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.awonar.app.ui.marketprofile.MarketProfileActivity
+import com.awonar.app.ui.profile.ProfileActivity
 import com.awonar.app.ui.search.adapter.SearchAdapter
 import com.awonar.app.ui.search.adapter.SearchItem
 
-@BindingAdapter("searchResult","viewModel")
+@BindingAdapter("searchResult", "viewModel")
 fun setSearchAdapter(
     recyclerView: RecyclerView,
     itemLists: MutableList<SearchItem>,
-    viewModel: SearchViewModel
+    viewModel: SearchViewModel,
 ) {
     if (recyclerView.adapter == null) {
         with(recyclerView) {
@@ -25,6 +26,11 @@ fun setSearchAdapter(
                         val newIntent =
                             Intent(recyclerView.context, MarketProfileActivity::class.java)
                         newIntent.putExtra(MarketProfileActivity.INSTRUMENT_EXTRA, id?.toInt())
+                        recyclerView.context.startActivity(newIntent)
+                    } else {
+                        val newIntent =
+                            Intent(recyclerView.context, ProfileActivity::class.java)
+                        newIntent.putExtra(ProfileActivity.EXTRA_USERID, id)
                         recyclerView.context.startActivity(newIntent)
                     }
                 }
